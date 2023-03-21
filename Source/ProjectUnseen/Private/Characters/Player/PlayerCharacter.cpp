b1e65130d82cc6bf19040997e77a0f0d85a604c6
img98 <nichy98@naver.com>
2023-03-21 23:46:08 +0900

    feat : Default Top-down shooter Framework

---
+++ Source/ProjectUnseen/Private/Characters/Player/PlayerCharacter.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/Player/PlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubSystems.h"
+#include "Kismet/KismetMathLibrary.h"
+
+APlayerCharacter::APlayerCharacter()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 1600.f;
+	SpringArm->SetRelativeRotation(FRotator(-75.f, 0.f, 0.f));
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 30.f;
+	SpringArm->bInheritYaw = false;
+
+	MainCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Main Camera"));
+	MainCamera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PlayerController = Cast<APlayerController>(Controller);
+
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(MappingContext, 0);
+	}
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	LookCursorDirection();
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MovementAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator ControlRotation = Controller->GetControlRotation();
+	const FRotator ControlYawRotation(0.f, ControlRotation.Yaw, 0.f);
+
+	const FVector ControlXDirection = FRotationMatrix(ControlYawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ControlXDirection, MovementVector.Y);
+	const FVector ControlYDirection = FRotationMatrix(ControlYawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(ControlYDirection, MovementVector.X);
+}
+
+void APlayerCharacter::LookCursorDirection()
+{
+	FHitResult HitResult;
+	PlayerController->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, HitResult);
+	const FVector MouseCursorVector(HitResult.Location.X, HitResult.Location.Y, 0.f);
+	const FVector ActorLocation(this->GetActorLocation());
+	const FVector CharacterVector(ActorLocation.X, ActorLocation.Y, 0.f);
+	const FRotator CharacterLookRotator = UKismetMathLibrary::FindLookAtRotation(CharacterVector, MouseCursorVector);
+	this->SetActorRotation(CharacterLookRotator);
+}
---
+++ Source/ProjectUnseen/Public/Characters/Player/PlayerCharacter.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/BaseCharacter.h"
+#include "InputActionValue.h"
+#include "PlayerCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class PROJECTUNSEEN_API APlayerCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	APlayerCharacter();
+
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	UPROPERTY(EditAnywhere, Category = ClassSetup, BlueprintReadWrite)
+	USpringArmComponent* SpringArm;
+	UPROPERTY(EditAnywhere, Category = ClassSetup, BlueprintReadWrite)
+	UCameraComponent* MainCamera;
+
+	APlayerController* PlayerController;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputMappingContext* MappingContext;
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* MovementAction;
+
+	void Move(const FInputActionValue& Value);
+
+	void LookCursorDirection();
+	//virtual void MoveForward(float Value);
+	//virtual void MoveRight(float Value);
+
+private:
+
+
+};
