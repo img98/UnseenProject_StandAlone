5907616ad65aff059c261acf14aef7ab92c2c512
img98 <nichy98@naver.com>
2023-07-04 18:23:02 +0900

    feat : BuildCancel기능 추가 및 GameMode에서 콜백형식으로 SpawningPool 조정

--- Source/ProjectUnseen/Private/SAGameMode.cpp
+++ Source/ProjectUnseen/Private/SAGameMode.cpp
@@ -16,3 +16,39 @@ void ASAGameMode::PostLogin(APlayerController* NewPlayer)
 	Super::PostLogin(NewPlayer);
 	UE_LOG(LogTemp, Warning, TEXT("PostLogin End"));
 }
+
+void ASAGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Start Initial Spawn
+	GetWorld()->GetTimerManager().SetTimer(
+		InitialSpawnTimer,
+		this,
+		&ASAGameMode::InitialSpawn,
+		5.f,
+		false
+	);
+
+	// Start Increase Pool Size
+	GetWorld()->GetTimerManager().SetTimer(
+		IncreasePoolSizeTimer,
+		this,
+		&ASAGameMode::IncreasePoolSize,
+		10.f, //DataAsset으로 관리하는게 나을지도?
+		true
+	);
+}
+
+
+void ASAGameMode::InitialSpawn()
+{
+	UE_LOG(LogTemp, Warning, TEXT("[GameMode] InitialSpawn Broadcasted!"));
+	OnInitialSpawn.Broadcast();
+}
+
+void ASAGameMode::IncreasePoolSize()
+{
+	UE_LOG(LogTemp, Warning, TEXT("[GameMode] IncreasePoolSize Broadcasted!"));
+	OnIncreasePoolSize.Broadcast();
+}
--- Source/ProjectUnseen/Public/SAGameMode.h
+++ Source/ProjectUnseen/Public/SAGameMode.h
@@ -6,16 +6,33 @@
 #include "GameFramework/GameModeBase.h"
 #include "SAGameMode.generated.h"
 
-/**
- * 
- */
+DECLARE_MULTICAST_DELEGATE(FInitialSpawnSignature);
+DECLARE_MULTICAST_DELEGATE(FIncreasePoolSizeSignature)
+
 UCLASS()
 class PROJECTUNSEEN_API ASAGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+	
 public:
 	ASAGameMode();
 
 	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY()
+	FTimerHandle InitialSpawnTimer;
+	UFUNCTION()
+	void InitialSpawn(); //콜백형태로 각SpawningPool들과 연결
+	UPROPERTY()
+	FTimerHandle IncreasePoolSizeTimer;
+	UFUNCTION()
+	void IncreasePoolSize();
+
+public:
+	FInitialSpawnSignature OnInitialSpawn;
+	FIncreasePoolSizeSignature OnIncreasePoolSize;
 };
